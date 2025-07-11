0c91983b99422eb02779e5f7dc7060b4b891aaed
Shadow737hub <nothinnothin162@gmail.com>
2025-07-11 18:40:58 +0530

    Create __init__.py

---
+++ IVANxMUSIC/__init__.py
@@ -0,0 +1,30 @@
+from IVANMUSIC.core.bot import IVAN
+from IVANMUSIC.core.dir import dirr
+from IVANMUSIC.core.git import git
+from IVANMUSIC.core.userbot import Userbot
+from IVANMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = SHUKLA()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "ll_DRAGON_MUSIC_BOT"  # connect music api key "Dont change it"
