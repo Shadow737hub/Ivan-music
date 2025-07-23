4b86c294ab5bc600b4714d0d5747682188972ac1
Shadow737hub <nothinnothin162@gmail.com>
2025-07-23 23:05:52 +0530

    _main_.py

--- IVANxMUSIC/_main_.py
+++ IVANxMUSIC/_main_.py
@@ -1,16 +1,61 @@
-from pyrogram import Client
-from config import Config
+import asyncio
+import importlib
 
-app = Client(
-    "music_bot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN
-)
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
-@app.on_message()
-async def hello(client, message):
-    await message.reply("🎵 Hello! I’m alive!")
+import config
+from IVANxMUSIC import LOGGER, app, userbot
+from SHUKLAMUSIC.core.call import IVAN
+from IVANxMUSIC.misc import sudo
+from IVANxMUSIC.plugins import ALL_MODULES
+from IVANxMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("IVANxMUSIC.plugins" + all_module)
+    LOGGER("IVANxMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await IVAN.start()
+    try:
+        await IVAN.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("IVANxMUSIC").error(
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗜𝗩𝗔𝗡 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+        )
+        exit()
+    except:
+        pass
+    await IVAN.decorators()
+    LOGGER("IVANxMUSIC").info(
+        "𝗠𝗔𝗗𝗘 𝗕𝗬 𝗜𝗩𝗔𝗡"
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("IVANxMUSIC").info("𝗦𝗧𝗢𝗣 𝗜𝗩𝗔𝗡 𝘅𝗔𝗛𝗔𝗗 𝗕𝗢𝗧..")
+
 
 if __name__ == "__main__":
-    app.run()
+    asyncio.get_event_loop().run_until_complete(init())aq
