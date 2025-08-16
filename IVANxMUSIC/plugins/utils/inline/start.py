f29bb6e798d470e1d1ad08b2378a322e51b4e0e3
Shadow737hub <nothinnothin162@gmail.com>
2025-08-16 13:55:50 +0530

    Update start.py

--- IVANxMUSIC/plugins/utils/inline/start.py
+++ IVANxMUSIC/plugins/utils/inline/start.py
@@ -1,6 +1,8 @@
 from pyrogram.types import InlineKeyboardButton
+
 import config
 from Ivanmusic import app
+
 
 def start_panel(_):
     buttons = [
@@ -8,13 +10,11 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page")  # About button
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
+
 
 def private_panel(_):
     buttons = [
@@ -24,41 +24,16 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_11"],
-                callback_data="about_page"
-            )
-        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="help_page_1")
-        ],
-    ]
-    return buttons
-
-def about_panel(_):
-    buttons = [
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
-        ]
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
+        ],
     ]
     return buttons
-
-def owner_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(text=_["S_H_1"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_H_2"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
-        ]
-    ]
-    return buttons 
