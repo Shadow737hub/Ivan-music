2bed06c189996d446fb2ac63d33cbffdb188f537
Shadow737hub <nothinnothin162@gmail.com>
2025-08-16 14:22:34 +0530

    Update playlist.py

--- IVANxMUSIC/plugins/play/playlist.py
+++ IVANxMUSIC/plugins/play/playlist.py
@@ -667,7 +667,7 @@ async def add_playlists(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     videoid = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
-    from Ivanmusic import YouTube
+    from shadow import YouTube
 
     _check = await get_playlist(user_id, videoid)
     if _check:
