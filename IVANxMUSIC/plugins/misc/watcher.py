96fec1941c2ad914c82232eb7c69d70202d75f4b
Shadow737hub <nothinnothin162@gmail.com>
2025-08-16 12:32:23 +0530

    Update watcher.py

--- IVANxMUSIC/plugins/misc/watcher.py
+++ IVANxMUSIC/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Shadow import app
-from Shadow.core.call import Hotty
+from Ivanmusic import app
+from Ivanmusic.core.call import Hotty
 
 welcome = 20
 close = 30
