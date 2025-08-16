739b2309b56b0bafb6777421f9b0a9cfd71f8fe4
Shadow737hub <nothinnothin162@gmail.com>
2025-08-16 13:13:27 +0530

    Update assistantdatabase.py

--- IVANxMUSIC/plugins/utils/database/assistantdatabase.py
+++ IVANxMUSIC/plugins/utils/database/assistantdatabase.py
@@ -45,7 +45,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from IVANMUSIC.core.userbot import assistants
+    from Ivanmusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
