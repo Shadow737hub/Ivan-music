e238d0b88e0021013bc31f177cf534aff4089d6b
Shadow737hub <nothinnothin162@gmail.com>
2025-08-16 13:09:15 +0530

    Update queue.py

--- IVANxMUSIC/plugins/utils/stream/queue.py
+++ IVANxMUSIC/plugins/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from IVANMUSIC.misc import db
-from IVANMUSIC.utils.formatters import check_duration, seconds_to_min
+from Ivanmusic.misc import db
+from Ivanmusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
